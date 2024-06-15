347e7ee4c8854e83c487f0672ef97ebf9fc604b5
Umar Farook <umarkhaji456@gmail.com>
2024-06-15 21:40:36 +0530

    started design of product-info

--- Frontend/src/Components/Collections/ProductInfo.jsx
+++ Frontend/src/Components/Collections/ProductInfo.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+import Collections from "./Collections";
 import "./styles.css";
+import "./InfoPage.css"
 const ProductInfo = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
@@ -25,16 +26,31 @@ const ProductInfo = () => {
 
   return (
     <div className="product-info-page">
-      <img
-        className="product-image"
-        src={product.image}
-        alt="Product Image"
-      />
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <span className="product-price">${product.price}</span>
-      {/* Add more details as needed */}
+      <div className="product-info">
+        <div className="product-preview">
+          <div className="image-preview">
+            <img src={product.image} alt="image Loading..." />
+          </div>
+          <div className="more-images"></div>
+        </div>
+        <div className="product-details">
+          <h1 className="p-title">{product.name}</h1>
+          <p className="p-category">{product.category}</p>
+          <label htmlFor="p-colors" className="p-colors-label">Colors</label>
+          <div id="p-colors" className="p-colors">
+
+          </div>
+          <div className="size-options"></div>
+          <h2 className="p-price">$ {product.price}</h2>
+          <button className="add-to-cart-btn">Add To cart</button>
+          <p className="p-description">{product.description}</p>
+        </div>
+      </div>
+      <div className="more-products">
+        <Collections />
+      </div>
     </div>
+    
   );
 };
 
